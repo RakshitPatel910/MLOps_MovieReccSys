e91c36169d118e73535bfcf584db25a28b1c28f0
Rakshit <rakshitpatel910@gmail.com>
2025-05-14 21:47:45 +0530

    backend connected to ml service in k8s

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/', userRouter);
-app.use('/auth/', authRouter);
+app.use('/backend/', userRouter);
+app.use('/backend/auth/', authRouter);
 
-const PORT = '3010';
+const PORT = '3000';
 
 app.listen(PORT, () => {
     console.log("server is running at port 3010");
